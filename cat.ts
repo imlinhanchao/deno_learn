e431ba331edc5b825daff452248b4ee9a122ec97
hancel.lin <imlinhanchao@foxmail.com>
2020-05-15 17:47:46 +0800

    day 1

---
+++ cat.ts
@@ -0,0 +1,8 @@
+// deno run --allow-read cat.ts curl.ts
+for (let i = 0; i < Deno.args.length; i++) {
+    let filename = Deno.args[i];
+    let file = await Deno.open(filename);
+    await Deno.copy(file, Deno.stdout);
+    file.close();
+}
+  
---
+++ curl.ts
@@ -0,0 +1,6 @@
+// deno run --allow-net=www.baidu.com curl.ts https://www.baidu.com
+const url = Deno.args[0];
+const res = await fetch(url);
+
+const body = new Uint8Array(await res.arrayBuffer());
+await Deno.stdout.write(body);
